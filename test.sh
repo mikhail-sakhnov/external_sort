b76b4c1de09499dc6446cf404645866223edccbf
Mikhail Sakhnov <sahnov.m@gmail.com>
2017-07-07 21:00:15 +0300

    added generation in test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,20 @@
-#!/usr/bin/env zsh
-if [ "$#" -ne 1 ]; then
-    echo "Usage: test.sh <filepath>"
-    exit;
-fi
+#!/usr/bin/env bash
+TEST_FILE=TestFile
 
+echo "Generating test file";
+python generator.py 100 1000 $TEST_FILE;
 echo "Sorting in memory";
-python sorter_in_memory.py $1;
+python sorter_in_memory.py $TEST_FILE;
 echo "External sorting with chunk size 10";
-python sorter.py $1 10;
+python sorter.py $TEST_FILE 10;
 echo "Dfff";
-diff "$1.sorted" "$1.sorted-inmemory";
+diff "$TEST_FILE.sorted" "$TEST_FILE.sorted-inmemory";
 
 
 echo "Sorting in memory REVERSED";
-python sorter_in_memory.py $1 true;
+python sorter_in_memory.py $TEST_FILE true;
 echo "External sorting with chunk size 10 REVERSED";
-python sorter.py $1 10 true;
+python sorter.py $TEST_FILE 10 true;
 echo "Dfff";
-diff "$1.sorted" "$1.sorted-inmemory";
+diff "$TEST_FILE.sorted" "$TEST_FILE.sorted-inmemory";
 
